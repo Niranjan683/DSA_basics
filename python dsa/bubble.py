08b6063a0f7f0dee80306049b29bace24ee64348
Niranjan683 <niranjan20032021@gmail.com>
2024-10-01 10:36:32 +0530

    Delete python dsa directory

--- python dsa/bubble.py
+++
@@ -1,8 +0,0 @@
-a=list(map(int,input('enter the values').split(',')))
-for i in range(len(a)):
-    for j in range(0,len(a)-1):
-        if a[j]>a[j+1]:
-            a[j],a[j+1]=a[j+1],a[j]
-        #print(a)
-    #print()
-print(a)
